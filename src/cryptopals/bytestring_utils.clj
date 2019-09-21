f320efbc9304863ac0e6e3b4f00e3df4f5d3cba3
Alex Frederiksen <ajfred154@gmail.com>
2019-09-21 15:56:23 -0400

    Make abstractions

--- src/cryptopals/bytestring_utils.clj
+++ src/cryptopals/bytestring_utils.clj
@@ -8,4 +8,17 @@
   [bs1 bs2]
   (map bit-xor bs1 bs2))
 
+(defn- byte-count-ones
+  [b]
+  (reduce + (map #(-> (bit-shift-right b %) 
+                      (bit-and 0x01)) (range 8))))
+
+(defn count-ones
+  [bs]
+  (transduce (map byte-count-ones) + 0 bs))
+
+(defn hamming-dist
+  [bs1 bs2]
+  (-> (xor bs1 bs2)
+      (count-ones)))
 
--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -2,7 +2,9 @@
   (:require [cryptopals.codecs :refer :all]
             [cryptopals.bytestring-utils :as b]
             [clojure.java.io :as io]
-            [clojure.string :as string])
+            [clojure.string :as string]
+            [clojure.test :refer :all]
+            [eftest.runner :as eftest])
   (:gen-class))
 
 ;;; ############################################################################
@@ -16,17 +18,23 @@
                    "6120706f69736f6e6f7573206d75" 
                    "7368726f6f6d"))
 
-
-(-> (decode-hex test-hex) (encode-base64))
+(deftest test-challenge1
+  (testing "Challenge 1"
+    (is (-> (decode-hex test-hex)
+            (encode-base64)
+            (= (str "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWt" 
+                    "lIGEgcG9pc29ub3VzIG11c2hyb29t"))))))
 
 ;;; CHALLENGE 2 ################################################################
 
 (def test-xor1 (decode-hex "1c0111001f010100061a024b53535009181c"))
 (def test-xor2 (decode-hex "686974207468652062756c6c277320657965"))
 
-(-> (b/test-xor1 test-xor2) (b/test-xor1) (encode-hex))
-
-(-> (encode-base64 test-xor1) (decode-base64) (encode-hex))
+(deftest test-challenge2
+  (testing "Challenge 2"
+    (is (-> (b/xor test-xor1 test-xor2) 
+            (encode-hex)
+            (= "746865206b696420646f6e277420706c6179")))))
 
 ;;; CHALLENGE 3 ################################################################
 
@@ -36,21 +44,12 @@
                             (decode-hex)))
 
 (def alpha? (set " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"))
-(alpha? \B)
-
-(defn char-freq
-  [s]
-  (transduce (filter alpha?) 
-             (completing (fn [[n m] k] 
-               (vector (inc n) (update m k (fnil inc 0)))))
-               [0 {}] (string/lower-case s)))
-
-(string/lower-case (apply str [\a \b \C]))
 
 (defn char-freq
   [s]
   (reduce (fn [[n m] k] 
-            (vector (inc n) (update m k (fnil inc 0)))) [0 {}] (string/lower-case (apply str s))))
+            (vector (inc n) (update m k (fnil inc 0)))) [0 {}] 
+          (string/lower-case (apply str s))))
 
 
 (defn update-values 
@@ -69,8 +68,6 @@
                       (#(filter alpha? %))
                       (char-freq-percent)))
 
-(print english-freq)
-
 (defn nilf
   [f k]
   (comp (fnil identity k) f))
@@ -78,7 +75,7 @@
 (defn score-map
   "Scores given map m by perfect map p using measure function f"
   [m p f]
-  (as-> (map #(apply f %) (map (juxt m (nilf p 0)) (keys m))) diffs
+  (as-> (map #(apply f %) (map (juxt m (nilf p -10)) (keys m))) diffs
     (reduce + 0.0 diffs)))
 
 
@@ -91,76 +88,127 @@
   [a b]
   (java.lang.Math/abs (- b a)))
 
-(score-map {\a 0.7 \b 0.1} {\b 0.4} l1-norm)
+(defn score-xor
+  ([bs xor]
+   (score-xor bs xor english-freq l1-norm))
+  ([bs xor good-freqs norm]
+   (-> (b/xor bs (repeat xor))
+       (encode-ascii)
+       (char-freq-percent)
+       (score-map good-freqs norm))))
 
-(defn single-byte-xor-decipher
-  "Given bytes and a map of average character frequencies (scorer),
-   finds most likely ciphers assuming a single byte was xor'ed with
-   everything"
-  [bs scorer norm]
-  (reduce (fn [m xor]
-            (let [xored (-> (b/xor bs (repeat xor))
-                            (encode-ascii))]
-              (assoc m [xor xored] 
-                     (score-map (char-freq-percent xored) scorer norm))))
-            {} (map unchecked-byte (range 256))))
-
-(defn sort-by-val
-  [m]
-  (into (sorted-map-by (fn [k1 k2]
-                   (compare (m k1) (m k2)))) m))
-
-(defn decipher-xor 
+(defn decrypt-single-xor
   [bs]
-  (-> (single-byte-xor-decipher bs english-freq l1-norm)
-      (sort-by-val)
-      (first)))
+  (apply min-key #(score-xor bs %) (range 256)))
 
-(decipher-xor test-single-cipher)
+;(as-> (decipher-xor test-single-cipher) xor
+;      (b/xor test-single-cipher (repeat xor))
+;      (encode-ascii xor))
+
+(deftest test-challenge3
+  (testing "Challenge 3"
+    (is (-> (decrypt-single-xor test-single-cipher)
+            (= 88)))))
 
 ;;; CHALLENGE 4 ################################################################
 
 (def xored-lines (as-> (io/resource "challenge4.txt") x
                        (slurp x)
                        (string/split x #"\n")
-                       (map decode-hex x)))
+                       (map decode-hex x)
+                       (vec x)))
 
-(def decipher-xored-lines 
+(def decrypt-all-xor 
+  "Returns [score xor line-number]"
   (-> (fn [lines]
-        (map decipher-xor xored-lines))
+        (map #(let [xor (decrypt-single-xor %1)
+                    score (score-xor %1 xor)]
+                (vector score xor %2)) 
+             lines (range)))
       (memoize)))
 
 (defn find-xor-encrypted
+  "Returns [score xor line-number]"
   [lines]
-  (let [results (decipher-xored-lines lines)
-        min-score (apply min (vals results))] 
-    (filter #(<= (val %) min-score) results)))
+  (let [decrypted (decrypt-all-xor lines)]
+    (apply min-key first decrypted)))
+
+;(decrypt-all-xor xored-lines)
 
 ; find xor'ed string
-(find-xor-encrypted xored-lines)
-
-; reverse to hex (found match on line 171)
-(-> (find-xor-encrypted xored-lines)
-    (first)
-    (key)
-    ((fn [[xor s]] 
-       (as-> (decode-ascii s) x
-         (b/xor x (repeat xor))
-         (encode-hex x)))))
+(deftest test-challenge4
+  (testing "Challenge 4"
+    (let [[score xor line-number] (find-xor-encrypted xored-lines)
+           line (xored-lines line-number)]
+      (is (= xor 53))
+      (is (= line-number 170)))))
 
 ;;; CHALLENGE 5 ################################################################
 
 (def test-xor-repeat (str "Burning 'em, if you ain't quick " 
                           "and nimble\nI go crazy when I hear a cymbal"))
 
+
 (defn xor-repeating
   [db kb]
-  (-> (b/xor db (cycle kb))))
+  (b/xor db (cycle kb)))
 
-(-> (xor-repeating (decode-ascii test-xor-repeat) (decode-ascii "ICE"))
-    (encode-base64))
-
+(deftest test-challenge5
+  (testing "Challenge 5"
+    (is (= (-> (xor-repeating (decode-ascii test-xor-repeat) (decode-ascii "ICE"))
+               (encode-hex))
+           (str "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d6"
+                "3343c2a26226324272765272a282b2f20430a652e2c652a3124"
+                "333a653e2b2027630c692b20283165286326302e27282f")))))
+        
 ;;; CHALLENGE 6 ################################################################
+
+(def repeat-xored-data (as-> (io/resource "challenge6.txt") x
+                        (slurp x)
+                        (string/split x #"\n")
+                        (string/join x)
+                        (decode-base64 x)))
+
+(defn test-keysize
+  [bs ksize n]
+  (let [dists (map #(/ (apply b/hamming-dist %) (double ksize)) 
+        (take n (partition 2 1 (partition ksize bs))))]
+    (/ (apply + dists) (double (count dists)))))
+
+(defn find-keysize
+  "Returns sorted vector of likely keysizes"
+  [bs]
+  (as-> (map #(vector (test-keysize bs % 20) %) (range 2 40)) x
+        (sort x)
+        (map second x)))
+
+(defn decrypt-blocks
+  [bs ksize]
+  (as-> (partition ksize bs) x
+    (apply map vector x)
+    (map decrypt-single-xor x)))
+
+(deftest test-challenge6
+  (testing "Challenge 6:"
+    (testing "Hamming distance"
+      (is (= (b/hamming-dist (decode-ascii "this is a test")
+                             (decode-ascii "wokka wokka!!!")) 
+             37)))
+    (testing "Repeating key decryption"
+      (let [ksize (-> (find-keysize repeat-xored-data)
+                      (first))
+            k (-> (decrypt-blocks repeat-xored-data ksize)
+                  (encode-ascii))] 
+        (is (= ksize 29))
+        (is (= k "Terminator X: Bring the noise"))))))
+
+(defn testall
+  []
+  (eftest/run-tests (eftest/find-tests *ns*)))
+
+;;; CHALLENGE 7 ################################################################
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
